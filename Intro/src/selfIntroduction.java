e7f7bba3eceb7517b6f45dfbca1677ad86a85db9
Lmori07 <33818263+Lmori07@users.noreply.github.com>
2022-05-20 15:35:39 -0400

    First commit for the Intro script from automation course selenium Java

---
+++ Intro/src/selfIntroduction.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class selfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Invoking the Browser
+		EdgeDriver driver = new EdgeDriver();
+	      //URL launch
+	      driver.get("https://www.tutorialspoint.com/index.htm");
+	}
+
+}
